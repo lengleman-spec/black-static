07ffe6bb6114fb2cabb079fddd88be45a9c9f52b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-27 18:24:21 -0500

    Add js to make the genre header slide in upon scrolling.

---
+++ script.js
@@ -0,0 +1,11 @@
+const sectionHeader = document.querySelector(".section-header");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+observer.observe(sectionHeader);
