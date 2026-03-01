4ea6c6553eefec7ea07be4b95ab2fb0bd472ee74
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-01 13:58:34 -0500

    Add hamburger menu for mobile. Add links inside.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const sectionHeader = document.querySelector(".section-header");
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".line-wrapper");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
